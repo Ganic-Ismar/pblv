3a570638382381528bfcbf45118ac4a4fe54cea7
MarMoh12 <marcel.mohrmann.mm@gmail.com>
2024-02-06 11:51:19 +0100

    Algo push v2

--- backend/algo.py
+++ backend/algo.py
@@ -7,20 +7,50 @@ class Fahrzeug:
         self.ladeleistung = ladeleistung
         self.fahrplan = []
 
-    def fahrplan_hinzufügen(self, zeit, ort, aktion, dauer):
-        self.fahrplan.append([zeit, ort, aktion, dauer])
+    def fahrplan_hinzufügen(self, datum_ankunft, zeit_ankunft, datum_abfahrt, zeit_abfahrt, ladebedarf):
+        self.fahrplan.append([datum_ankunft, zeit_ankunft, datum_abfahrt, zeit_abfahrt, ladebedarf])
 
     def fahrplan_anzeigen(self):
         print("Fahrplan für", self.modell)
-        print("Zeit\tOrt\tAktion\tDauer")
+        print("Datum Ankunft\tZeit Ankunft\tDatum Abfahrt\tZeit Abfahrt\tLadebedarf")
         for eintrag in self.fahrplan:
             print(*eintrag, sep='\t')
 
 
-# Beispielverwendung:
-auto = Fahrzeug("Tesla Model S", "Elektro", 75, 20, 50)
-auto.fahrplan_hinzufügen("08:00", "Berlin", "Fahren", "1 Stunde")
-auto.fahrplan_hinzufügen("09:00", "Hamburg", "Laden", "30 Minuten")
-auto.fahrplan_hinzufügen("09:30", "Hamburg", "Fahren", "2 Stunden")
-auto.fahrplan_hinzufügen("11:30", "Bremen", "Fahren", "1 Stunde")
-auto.fahrplan_anzeigen()
+# Klasse für die Prognose
+class Prognose:
+    def __init__(self):
+        self.prognose = []
+
+    def prognose_hinzufügen(self, datum, start, ende, wert):
+        self.prognose.append([datum, start, ende, wert])
+
+    def prognose_anzeigen(self):
+        print("Prognose")
+        print("Datum\tStart\tEnde\tWert\t")
+        for eintrag in self.fahrplan:
+            print(*eintrag, sep='\t')
+
+
+# Main:
+# Auto1 anlegen
+auto1 = Fahrzeug("BMW i4", "Vollelektrisch", 60, 14.4, 4)
+auto1.fahrplan_hinzufügen("01.01.2020", "14:40", "02.01.2020", "06:00", "25")
+auto1.fahrplan_hinzufügen("02.01.2020", "12:45", "03.01.2020", "06:10", "21")
+auto1.fahrplan_hinzufügen("03.01.2020", "14:00", "04.01.2020", "11:05", "18")
+auto1.fahrplan_hinzufügen("04.01.2020", "19:10", "06.01.2020", "17:20", "60")
+auto1.fahrplan_anzeigen()
+
+#Auto2 anlegen
+auto2 = Fahrzeug("Tesla Model 3", "Vollelektrisch", 80, 16.3, 4)
+auto2.fahrplan_hinzufügen("01.01.2020", "13:00", "02.01.2020", "17:00", "13")
+auto2.fahrplan_hinzufügen("02.01.2020", "21:00", "03.01.2020", "14:11", "21")
+auto2.fahrplan_hinzufügen("03.01.2020", "15:05", "03.01.2020", "17:00", "5")
+auto2.fahrplan_hinzufügen("03.01.2020", "19:10", "04.01.2020", "15:30", "30")
+auto2.fahrplan_hinzufügen("04.01.2020", "19:00", "05.01.2020", "17:25", "62")
+auto2.fahrplan_hinzufügen("05.01.2020", "21:10", "06.01.2020", "14:35", "12")
+auto2.fahrplan_anzeigen()
+
+#Prognose erzeugen
+prognose = Prognose()
+prognose.prognose_hinzufügen
