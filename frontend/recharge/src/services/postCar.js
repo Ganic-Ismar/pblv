70c8b0f8a5d9080338101afe6459932e14cae0c6
Moritz Wagner <info@moritzwagner.eu>
2024-02-08 13:37:02 +0100

    Update API endpoints for posting and fetching cars

--- frontend/recharge/src/services/postCar.js
+++ frontend/recharge/src/services/postCar.js
@@ -3,7 +3,8 @@ import Car from '../objects/Car.obj.js';
 function postCar(Car) {
     // Make a POST request to the server with the car data
     const api_host = process.env.REACT_APP_API_HOST;
-    fetch(api_host+'/api/cars', {
+    const api_port = process.env.REACT_APP_API_PORT;
+    fetch('http://'+api_host+':'+api_port+'/api/cars', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
--- frontend/recharge/src/utils/dataFetcher.js
+++ frontend/recharge/src/utils/dataFetcher.js
@@ -5,7 +5,9 @@ export async function fetchPlanData() {
 }
 
 export async function fetchCarData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const api_host = process.env.REACT_APP_API_HOST;
+    const api_port = process.env.REACT_APP_API_PORT;
+    const response = await fetch('http://'+api_host+':'+api_port+'/cars/cars');
     const data = await response.json();
     return data;
 }
